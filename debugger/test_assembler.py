2049d24e025f339a4a2e9a771888c6a151a1bbac
Mayssam Naji <mayssamnaji@icloud.com>
2023-11-28 15:28:26 +0100

    test add 2 nb

---
+++ debugger/test_assembler.py
@@ -0,0 +1,28 @@
+import subprocess
+import pytest
+
+def run_assembler(input_as, output_mx):
+    cmd = [
+        'python',
+        'debugger/assembler.py',
+        input_as,
+        output_mx
+    ]
+    subprocess.run(cmd, check=True)
+
+def test_addTwoNumbers():
+    input_as = 'exercise 1/addTwoNumbers.as'
+    output_mx = 'exercise 1/addTwoNumbers.mx'
+    expected_output_mx = 'exercise 1/expected_addTwoNumbers.mx'
+
+     # Define the expected output
+
+    run_assembler(input_as, output_mx)
+
+    with open(output_mx, 'r') as file:
+        output = file.read()
+    
+    with open(expected_output_mx, 'r') as file:
+        expected_output = file.read()
+    
+    assert output == expected_output

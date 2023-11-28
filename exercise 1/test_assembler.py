e596cbe1982e243fb279a6d2889c1d87a4ab80bb
Mayssam Naji <mayssamnaji@icloud.com>
2023-11-28 15:37:28 +0100

    move test

--- exercise 1/test_assembler.py
+++ exercise 1/test_assembler.py
@@ -13,7 +13,24 @@ def run_assembler(input_as, output_mx):
 def test_addTwoNumbers():
     input_as = 'exercise 1/addTwoNumbers.as'
     output_mx = 'exercise 1/addTwoNumbers.mx'
-    expected_output_mx = 'exercise 1/expected_addTwoNumbers.mx'
+    expected_output_mx = 'exercise 1/test_addTwoNumbers.mx'
+
+     # Define the expected output
+
+    run_assembler(input_as, output_mx)
+
+    with open(output_mx, 'r') as file:
+        output = file.read()
+    
+    with open(expected_output_mx, 'r') as file:
+        expected_output = file.read()
+    
+    assert output == expected_output
+
+def test_subtractTwoNumbers():
+    input_as = 'exercise 1/subtractTwoNumbers.as'
+    output_mx = 'exercise 1/subtractTwoNumbers.mx'
+    expected_output_mx = 'exercise 1/test_subtractTwoNumbersmx'
 
      # Define the expected output
 

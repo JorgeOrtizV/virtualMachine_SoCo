28aceda4d4f98725e0fb43ab8e297bdba0e3c211
panagiotis patsias <patsias.panagiotis@gmail.com>
2023-11-29 02:45:54 +0100

    new_tests

---
+++ exercise 1/StoreLoadFromMemory.as
@@ -0,0 +1,5 @@
+ldc R0 15      
+ldc R1 5       
+str R0 R1      
+prm R1  
+hlt 
--- exercise 1/test_assembler.py
+++ exercise 1/test_assembler.py
@@ -1,5 +1,6 @@
 import subprocess
 import pytest
+
 def run_assembler(input_as, output_mx):
     # TODO: make this robust to run in either win, linux, mac
     cmd = [
@@ -31,6 +32,40 @@ def test_subtractTwoNumbers():
     input_as = 'subtractTwoNumbers.as'
     output_mx = 'subtractTwoNumbers.mx'
     expected_output_mx = 'test_subtractTwoNumbers.txt'
+
+     # Define the expected output
+
+    run_assembler(input_as, output_mx)
+
+    with open(output_mx, 'r') as file:
+        output = file.read()
+    
+    with open(expected_output_mx, 'r') as file:
+        expected_output = file.read()
+    
+    assert output == expected_output
+
+def test_UseOf_beq():
+    input_as = 'test_UseOf_beq.as'
+    output_mx = 'test_UseOf_beq.mx'
+    expected_output_mx = 'test_UseOf_beq.txt'
+
+     # Define the expected output
+
+    run_assembler(input_as, output_mx)
+
+    with open(output_mx, 'r') as file:
+        output = file.read()
+    
+    with open(expected_output_mx, 'r') as file:
+        expected_output = file.read()
+    
+    assert output == expected_output
+
+def test_Loop():
+    input_as = 'Loop.as'
+    output_mx = 'Loop.mx'
+    expected_output_mx = 'test_Loop.txt'
 
      # Define the expected output
 
